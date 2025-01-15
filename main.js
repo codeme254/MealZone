20426086a28110fea26b45bd41453ddaac8723f1
codeme254 <otwomadennis365@gmail.com>
2025-01-15 09:11:22 +0300

    reveal header on scroll

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
 const toggleMenuBtn = document.getElementById("toggle-menu-btn");
 const nav = document.getElementById("nav");
 
+// toggle menu
 toggleMenuBtn.addEventListener("click", () => {
   nav.classList.toggle("header-nav-active");
 });
+
+// show header
+const header = document.getElementById("header");
+const hero = document.getElementById("hero");
+
+const heroSectionObserver = new IntersectionObserver(
+  (entries, heroSectionObserver) => {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) {
+        header.classList.add("header-visible");
+      } else {
+        header.classList.remove("header-visible");
+      }
+    });
+  },
+  {},
+);
+
+heroSectionObserver.observe(hero);
